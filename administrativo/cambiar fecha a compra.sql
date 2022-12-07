6a32ecd848b7167f7383b39f1416003f30f0c156
Wilson Blanco <blancowilson83@gmail.com>
2022-12-07 10:58:23 -0400

    Cambiar fecha a compra

--- administrativo/cambiar fecha a compra.sql
+++ administrativo/cambiar fecha a compra.sql
@@ -6,37 +6,33 @@ declare @NumeroD varchar(15),
 		@tipopago varchar(2),
 		@nrolote varchar (15)
 SET @Numerod='00206316'
+set @codprov ='HYL001'
+Set @Tipocom='H'
 
 
-select  fechai,fechae,fechat,*   From    SACOMP where NumeroD=@NumeroD and TipoCom=@Tipocom and CodProv=@codprov
--- items de compra
-select  fechae,*    From   SAITEMCOM  where NumeroD=@NumeroD and TipoCom=@Tipocom and CodProv=@codprov
+--select  fechai,fechae,fechat,*   From    SACOMP where NumeroD=@NumeroD and TipoCom=@Tipocom and CodProv=@codprov
+---- items de compra
+--select  fechae,*    From   SAITEMCOM  where NumeroD=@NumeroD and TipoCom=@Tipocom and CodProv=@codprov
 
-select  *    From   SASEPRCOM  where NumeroD=@NumeroD and TipoCom=@Tipocom and CodProv=@codprov
+--select  *    From   SASEPRCOM  where NumeroD=@NumeroD and TipoCom=@Tipocom and CodProv=@codprov
 
-select  *    From   SATAXITC where NumeroD=@NumeroD and TipoCom=@Tipocom and CodProv=@codprov
+--select  *    From   SATAXITC where NumeroD=@NumeroD and TipoCom=@Tipocom and CodProv=@codprov
 
-select  *    From   SATAXCOM where NumeroD=@NumeroD and TipoCom=@Tipocom and CodProv=@codprov
+--select  *    From   SATAXCOM where NumeroD=@NumeroD and TipoCom=@Tipocom and CodProv=@codprov
 
-select  fechae,*    From   SAPAGCXP where NroPpal=(select NroUnico from SAACXP where NumeroN=@NumeroD  and CodProv=@codprov) and NumeroD=@NumeroD
+--select  fechae,*    From   SAPAGCXP where NroPpal=(select NroUnico from SAACXP where NumeroN=@NumeroD  and CodProv=@codprov) and NumeroD=@NumeroD
 
-select  fechae,*    From    SAPAGCXP where NroPpal=(select NroUnico from SAACXP where NumeroN=@NumeroD  and CodProv=@codprov) and NumeroD=@NumeroD
+--select  fechae,*    From    SAPAGCXP where NroPpal=(select NroUnico from SAACXP where NumeroN=@NumeroD  and CodProv=@codprov) and NumeroD=@NumeroD
 
-select  fechai,fechae,fechat,*    From   SAACXP where NumeroD=@NumeroD and CodProv=@codprov
+--select  fechai,fechae,fechat,*    From   SAACXP where NumeroD=@NumeroD and CodProv=@codprov
 
-select  fechai,fechae,fechat,*    From   SAACXP where NumeroN=@NumeroD and CodProv=@codprov
-
-
+--select  fechai,fechae,fechat,*    From   SAACXP where NumeroN=@NumeroD and CodProv=@codprov
 
 
-
-
-update saacxp 
-set FechaE ='20220729 00:00:00.000', FechaI='20220718 00:00:00.000'
-where tipocxp =10 and Numerod=@NumeroD
-
-
+--update saacxp 
+--set FechaT ='20221130 00:00:00.000', FechaI='20221130 00:00:00.000'
+--where tipocxp =10 and Numerod=@NumeroD
 
 update SACOMP
-set FechaE ='20220729 00:00:00.000', FechaI='20220718 00:00:00.000'
+set FechaT ='20221130 00:00:00.000'
 where NumeroD = @NumeroD and CodProv =@codprov
